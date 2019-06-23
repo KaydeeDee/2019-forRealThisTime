14f61c7cfbafde6ccc446c0c9c185fde1a43d90f
KaydeeDee <kd.dangelo@gmail.com>
2019-06-22 20:33:24 -0400

    init

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const lightbulbBottomSVG = document.querySelector('.lightbulb-bottom');
+// const test = lightbulbBottomSVG.getTotalLength();
+$(function () {
+  $("#fontButton").click(function () {
+    $("#header").toggleClass("dxyFont");
+    $("#body").toggleClass("dxyFont");
+    $(this).text(function (_, oldText) {
+      return oldText === 'Click here to return to original font' ? 'Click here to use Dxy Font' : 'Click here to return to original font';
+    });
+  });
+});
